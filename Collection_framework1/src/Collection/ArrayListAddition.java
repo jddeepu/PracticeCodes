e5bbb5768dc141ed62b5e92226e0cf25909d6289
jddeepu <dg6732049@gmail.com>
2023-07-08 14:13:58 +0530

    One file added

---
+++ Collection_framework1/src/Collection/ArrayListAddition.java
@@ -0,0 +1,17 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class ArrayListAddition {
+   
+	 public static void main(String[] args) {
+		
+		 ArrayList<Integer> arrList = new ArrayList<>();
+		 arrList.add(1);
+		 arrList.add(2);
+		 
+		 
+		 System.out.println(arrList);
+		 
+	}
+}
